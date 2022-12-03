e39b920868b4b788de640b0fa787da31e4c7e117
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-12-04 01:45:17 +0530

    Added NameSpace

--- Assets/Scripts/Managers/SubLevelManager.cs
+++ Assets/Scripts/Managers/SubLevelManager.cs
@@ -1,24 +1,27 @@
 using UnityEngine;
 using Events;
 
-public class SubLevelManager : GenericSingleton<SubLevelManager>
+namespace LevelManager
 {
-    [SerializeField] private GameObject[] subLevels;
-    int currInd = 0;
+    public class SubLevelManager : GenericSingleton<SubLevelManager>
+    {
+        [SerializeField] private GameObject[] subLevels;
+        int currInd = 0;
 
-    private void Start()
-    {
-        EventService.Instance.PackageSubmitted += ActivateNextSubLevel;
-        for(int i=1;i<subLevels.Length;i++)
-            subLevels[i].SetActive(false);
-        subLevels[0].SetActive(true);
-    }
-    private void ActivateNextSubLevel()
-    {
-        if ((currInd + 1) == subLevels.Length)
-            return;
-        subLevels[currInd].SetActive(false);
-        currInd++;
-        subLevels[currInd].SetActive(true);
+        private void Start()
+        {
+            EventService.Instance.PackageSubmitted += ActivateNextSubLevel;
+            for (int i = 1; i < subLevels.Length; i++)
+                subLevels[i].SetActive(false);
+            subLevels[0].SetActive(true);
+        }
+        private void ActivateNextSubLevel()
+        {
+            if ((currInd + 1) == subLevels.Length)
+                return;
+            subLevels[currInd].SetActive(false);
+            currInd++;
+            subLevels[currInd].SetActive(true);
+        }
     }
 }
